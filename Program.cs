51149078466f5c256aa46e3d9a5a777dbe253949
Aleksandr <cross_comp@hotmail.com>
2024-05-25 14:54:02 +0300

    added code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый
+// массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив
+// можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении
+// не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] array = ["Hello", "my", "crazy", "word", "!"];
+string[] new_array = new string[array.Length];
+
+int index = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        new_array[index] = array[i];
+        index++;
+    }
+}
+
+Console.Write("[");
+for (int i = 0; i < index; i++)
+{
+    Console.Write($"'{new_array[i]},'");
+}
+Console.Write("]");
+
+
